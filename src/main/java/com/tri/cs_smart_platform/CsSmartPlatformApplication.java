64af05bc46f8ae86755a7f25aa6da47b934999df
Minh Tri <minhtri290104@gmail.com>
2026-03-04 13:28:45 +0700

    init spring boot project

---
+++ src/main/java/com/tri/cs_smart_platform/CsSmartPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.tri.cs_smart_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsSmartPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsSmartPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tri/cs_smart_platform/CsSmartPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tri.cs_smart_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsSmartPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
